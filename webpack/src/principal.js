f46892cba5bf8509dc10a6b2771f456832e6164a
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-06-15 01:17:08 +0000

    webpack

---
+++ webpack/src/principal.js
@@ -0,0 +1,4 @@
+import Pessoa from './pessoa'
+
+const p1 = new Pessoa
+console.log(p1.cumprimentar())
---
+++ webpack/src/webpack.config.js
@@ -0,0 +1,7 @@
+const webpack = require('webpack')
+
+module.exports = {
+    name: 'cod3r-webpack',
+    entry: './src/principal.js',
+    mode: 'development',
+}
