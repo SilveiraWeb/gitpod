2da579dd74aeeda41fa8ddc684de1dadf9cb5270
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-06-21 21:41:05 +0000

    react

---
+++ nlw-react/letmeask/src/App.tsx
@@ -0,0 +1,13 @@
+import {Button} from "./components/Button"
+
+function App() {
+  return (
+    <div>
+      <Button text="Botao 1"/>
+      <Button>Clique aqui</Button>
+      <Button>0</Button>
+    </div>
+  );
+}
+
+export default App;
