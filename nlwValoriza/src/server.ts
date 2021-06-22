a02d251745743fa66ae9d396eec0b1cbfe7f87c5
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-06-22 01:08:19 +0000

    database

--- nlwValoriza/src/server.ts
+++ nlwValoriza/src/server.ts
@@ -1,18 +1,12 @@
 // caso apareca os '...' no nome do modulo sera 
 // necessario instalar a biblioteca de tipagem "@types/express"
 import express from "express";
+import "reflect-metadata";
+import "./database"; //  sqlite connection
 
 //@types/express
 const app = express();
 
-/**
- * - Methods -
- * GET    => Busca informações
- * POST   => Inserir informações
- * PUT    => Alterar uma informação
- * DELETE => Apagar um dado
- * PATH   => Alterar uma informação especifica
- */
 app.get("/test", (req, res) => {
     return res.send("Hello NLW");
 });
@@ -24,3 +18,23 @@ app.post("/test-post", (req, res) => {
 app.listen(3000, () => {
   console.log("Server is running...");
 });
+
+
+/**
+ * - Methods -
+ * GET    => Busca informações
+ * POST   => Inserir informações
+ * PUT    => Alterar uma informação
+ * DELETE => Apagar um dado
+ * PATH   => Alterar uma informação especifica
+ */
+/**
+ * Tipos  de paramêtros
+ * Routes Params  => http://localhost:3000/products/78845548874555
+ * Query Params   => http://localhost:3000/products?name=teclado&descriiption=tecladobom&price=low
+ * Body Params    =>  {
+ *  "name": "teclado,
+ * "description": "tecladobom",
+ * "price": "low"
+ * " * }
+ */
