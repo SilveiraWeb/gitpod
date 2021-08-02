1f926e92498e10414c3783eee9be87c27686b157
Silveira Guimaraes <silveira_oi@yahoo.com.br>
2021-08-02 20:11:42 +0000

    Teste

---
+++ objects/oo.js
@@ -0,0 +1,17 @@
+// codigo nao executavel
+
+// Procedural classico
+proccessamento(valor1, valor2, valor3); 
+
+// OOJS
+objeto = {
+  valor1, 
+  valor2, 
+  valor3,
+  proccessamento(){
+    // ...
+  }
+}
+// foco passaou a ser objeto
+objeto.proccessamento()
+// Principios importantes
